cfbaf5bb3e87d06c6292cfaff3f219016b1cb8f0
sarpong <abasimisarpong@rocketmail.com>
2019-04-26 16:53:25 +0100

    Added a self.create to  model

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -8,7 +8,17 @@ class Bookmark
     else  
       connection = PG.connect :dbname => 'bookmark_manager'
     end
-    result = connection.exec("SELECT * FROM bookmarks")
+    result = connection.exec("SELECT * FROM bookmarks;")
     result.map { |each_bookmark| each_bookmark['url'] }
+  end
+
+
+  def self.create(url:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else  
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+      connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}');")
   end
 end
